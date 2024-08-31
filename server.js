cb7b9d520f91721c323f5f5a6d23f4224a797bdc
vishal-9368 <vshalsha@1234@gmail.com>
2024-08-31 23:05:01 +0530

    vi

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-const job = schedule.scheduleJob("27 17 * * *", async function (fireDate) {
+const job = schedule.scheduleJob("40 17 * * *", async function (fireDate) {
   console.log(
     "This job was supposed to run at " +
       fireDate +
